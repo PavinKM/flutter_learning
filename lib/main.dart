e321f2e0affcb765956f0cce372775cb90b0aba1
PavinKM <pavinyagava@gmail.com>
2025-07-16 13:39:15 +0530

    16/7 afternoon commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,36 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
+void main(){  //entry point
+    runApp(MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
+        appBar: AppBar(
+          title: Text(
+              "Doctor App",
+              style: TextStyle(color: Colors.white),
+            ),
+          backgroundColor: Colors.blue,
+        )
+      )
+    ));
 }
+
+
+
+// void main() {
+//   runApp(const MainApp());
+// }
+
+// class MainApp extends StatelessWidget {
+//   const MainApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       home: Scaffold(
+//         body: Center(
+//           child: Text('Hello World!'),
+//         ),
+//       ),
+//     );
+//   }
+// }
